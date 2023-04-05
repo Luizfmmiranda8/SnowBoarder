f7b177e4f71723ee8feb922e2af07322dc0abdb8
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-04-05 11:56:52 -0300

    Improve gameplay
    Place sprites to create a more beatiful level, create some particle effects and create a boost mechanic to improve gameplay

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,14 @@ public class CrashDetector : MonoBehaviour
 {
     #region VARIABLES
     [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem crashEffect;
     #endregion
     #region EVENTS
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", delay);
         }
     }
--- SnowBoarder/Assets/Scripts/FinishLine.cs
+++ SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -5,12 +5,14 @@ public class FinishLine : MonoBehaviour
 {
     #region VARIABLES
     [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     #endregion
     #region EVENTS
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", delay);
         }
     }
